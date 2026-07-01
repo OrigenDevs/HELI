76d3270da40fe4c1f8dcbbf7487d13fa2ea98fa9
Juan Salguero <jrnmslgr@gmail.com>
2026-06-30 23:25:31 -0500

    Enemigo: animator manual + null checks y warning

--- Assets/HELI/CODIGO/Scripts_2DComic/Enemigo.cs
+++ Assets/HELI/CODIGO/Scripts_2DComic/Enemigo.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class Enemigo : MonoBehaviour
 {
     [Header("Vida")]
     public float salud = 1f;
 
-    private Animator animator;
+    [Header("Animaciones")]
+    public Animator animator;
+
     private Collider2D col;
     private static readonly int ParamGolpeado = Animator.StringToHash("golpeado");
     private static readonly int ParamDerrotado = Animator.StringToHash("derrotado");
 
     void Awake()
     {
-        animator = GetComponent<Animator>();
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
         col = GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+        if (animator == null)
+            Debug.LogWarning("Enemigo sin Animator. Arrástralo en el campo animator.", this);
     }
 
     public void RecibirDano(float dano)
@@ -23,14 +31,16 @@ public class Enemigo : MonoBehaviour
 
         if (salud <= 0f)
             Derrotar();
-        else
+        else if (animator != null)
             animator.SetTrigger(ParamGolpeado);
     }
 
     void Derrotar()
     {
-        animator.SetTrigger(ParamDerrotado);
-        col.enabled = false;
+        if (animator != null)
+            animator.SetTrigger(ParamDerrotado);
+        if (col != null)
+            col.enabled = false;
         Destroy(gameObject, 1f);
     }
 }
